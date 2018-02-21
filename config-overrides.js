8945a9d071544b26a5643f0be9e6d53d96edcb66
katkase <fraloschi@gmail.com>
2018-02-21 17:25:33 +0100

    fixed modifyVars not working

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,48 @@
-const { injectBabelPlugin, compose } = require('react-app-rewired');
+const path = require("path");
+
+const { injectBabelPlugin, compose } = require("react-app-rewired");
+//const rewireLess = require("react-app-rewire-less");
 const rewireLess = require("react-app-rewire-less-modules");
+const rewireVendorSplitting = require("react-app-rewire-vendor-splitting");
+
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = function override(config, env) {
-  config = rewireLess(config, env);
-  config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
-  config = rewireLess.withLoaderOptions({
-    modifyVars: { "@primary-color": "#1DA57A" },
-  })(config, env);
-  return config;
+  if (env === "development") {
+    config = injectBabelPlugin(["dva-hmr"], config);
+  }
+
+  config = injectBabelPlugin(
+    ["import", { libraryName: "antd", style: true, libraryDirectory: "es" }],
+    config
+  );
+
+  if (env === "production") {
+    config.plugins.push(
+      new BundleAnalyzerPlugin({
+        analyzerMode: "static",
+        openAnalyzer: false
+      })
+    );
+  }
+
+  const rewires = compose(
+    rewireLess.withLoaderOptions(
+      `${env === "production" ? "app" : "[local]"}-[hash:base64:8]`,
+      {
+        modifyVars: {
+          "@primary-color": "#ff79c6",
+          "@info-color": "#61dafb",
+          "@layout-body-background": "#EEEEF0",
+          "@layout-header-background": "#fff",
+          "@layout-sider-background": "#282A36",
+          "@menu-dark-bg": "#282A36",
+          "@menu-dark-submenu-bg": "#21222c",
+        }
+      }
+    ),
+    rewireVendorSplitting
+  );
+
+  return rewires(config, env);
 };
